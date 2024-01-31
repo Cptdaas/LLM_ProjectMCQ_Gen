e45c41e969994e08b5a9e9e565ca777d2f5ff090
Vinod Tiwari <100059115+Cptdaas@users.noreply.github.com>
2024-01-31 14:51:12 +0530

    file with fullopenaisupportpaid
    I am adding here new openai key paid. running state.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages,setup
 
 setup(
-    name="mcqgenerator",
+    name='mcqgenrator',
     version='0.0.1',
-    auther='vinod tiwari',
-    author_email='cpt1995daas@gmail.com',
-    install_reqires=["opeai","lamngchain","streamlit","python-dotenv","PyPDF2"],
+    author='sunny savita',
+    author_email='sunny.savita@ineuron.ai',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
